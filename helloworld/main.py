108da014d1bf81c28d36d9421c2a292cc12316e7
Andre Placido <andre.placido@gmail.com>
2020-10-05 16:34:50 +0100

    allow cors resquest in the hello_world cloud function

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,4 +1,16 @@
 def hello_world(request):
+    if request.method == 'OPTIONS':
+        #Resolvendo o problema de CORS
+        headers = {
+            'Access-Control-Allow-Origin':'*', # especifique o dom'inio para restringir ou use * para todos.
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600' # cache temporario para o header de 1 hora
+        }
+        return '', 204, headers
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
     request_args = request.args
     request_json = request.get_json(silent=True) # sem jason send null => "silent = True"
     if request_args and 'name' in request_args and 'lastname' in request_args:
@@ -10,4 +22,4 @@ def hello_world(request):
     else:
         name = 'world'
         lastname = ''
-    return f'Hello {name} {lastname}'
+    return f'Hello {name} {lastname}', 200, headers
